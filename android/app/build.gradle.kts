e49a29716b8f29888843c44475dd138ba78bfa4d
Muhammad Umar <sajosinsss@gmail.com>
2025-07-25 14:09:05 +0500

    1st

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,35 +3,33 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    // Add the Google services Gradle plugin
-    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.tmc.fluttertemplate"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
-
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
+    
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-
+    
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.tmc.fluttertemplate"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
+    
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
@@ -46,15 +44,5 @@ flutter {
 }
 
 dependencies {
-    // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
-
-    // Firebase Authentication
-    implementation("com.google.firebase:firebase-auth")
-
-    // Firebase Analytics (optional)
-    implementation("com.google.firebase:firebase-analytics")
-
-    // Firebase Firestore (for user data)
-    implementation("com.google.firebase:firebase-firestore")
+    // No Firebase dependencies needed for local storage app
 }
